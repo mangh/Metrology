6a6eb37cbf57b013a06f3b87d8742a37f33e95f3
Marek <man130117@outlook.com>
2021-12-11 11:32:44 +0100

    Demo/Bullet project introduced

---
+++ Demo/Bullet/Bullet.Measured.cs
@@ -0,0 +1,44 @@
+﻿namespace Bullet.Measured
+{
+    public class Calculator
+    {
+        public readonly Meter_Sec2 g = (Meter_Sec2)9.80665; // the gravitational acceleration
+        public readonly Meter_Sec v;    // the velocity at which the projectile is launched
+        public readonly Meter h;        // the initial height of the projectile
+
+        public Calculator(Meter height, Meter_Sec velocity)
+        {
+            v = velocity;
+            h = height;
+        }
+
+        public (Degree, Second, Meter, Meter) CalculateRange(Degree slope)
+        {
+            // the angle (in Radians) at which the projectile is launched
+#if DIMENSIONAL_ANALYSIS
+            Radian angle = Radian.From(slope);
+#else
+            Radian angle = Demo.UnitsOfMeasurement.Radian.FromDegree(slope);
+#endif
+
+            // the vertical component of the velocity
+            Meter_Sec vy = v * Sin(angle);
+
+            // the time it takes for the projectile to finish its trajectory:
+            Second tmax = (vy + Sqrt(vy * vy + 2.0 * g * h)) / g;
+
+            // the total horizontal distance traveled by the projectile
+            Meter xmax = v * Cos(angle) * tmax;
+
+            // maximum vertical displacement of the projectile
+            Meter ymax = h;
+            for (Second t = (Second)0.0; t < tmax; t++)
+            {
+                Meter y = h + (vy - g * t) * t / 2.0;
+                if (y > ymax) ymax = y;
+            }
+
+            return (slope, tmax, xmax, ymax);
+        }
+    }
+}
---
+++ Demo/Bullet/Sampler.Measured.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace Bullet.Measured
+{
+    public class Sampler : Calculator
+    {
+        public List<(Degree, Second, Meter, Meter)> results;
+        public Timer<Degree, (Degree, Second, Meter, Meter)> timer;
+        public Benchmark benchmark;
+
+        public Sampler(Meter height, Meter_Sec velocity) :
+            base(height, velocity)
+        {
+            results = new List<(Degree, Second, Meter, Meter)>(1000);
+            benchmark = new Benchmark();
+            timer = new Timer<Degree, (Degree, Second, Meter, Meter)>(
+                (Degree min, Degree max, Degree step) =>
+                {
+                    results.Clear();
+                    for (Degree slope = min; slope < max; slope += step) results.Add(CalculateRange(slope));
+                    return results;
+                }
+            );
+        }
+
+        public List<(Degree, Second, Meter, Meter)> Sample(Degree min, Degree max, Degree step)
+        {
+            var result = timer.WatchComputation(min, max, step);
+            benchmark.Add(timer.elapsed);
+            return result;
+        }
+    }
+}
